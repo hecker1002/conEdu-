2dabbb24892197f154aa936860f95ac7441af92f
riziuzi <rishi18december@gmail.com>
2023-03-18 17:00:03 +0530

    Commit question anwer completed

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,21 @@ const passport = require('passport');
 router.get('/register', (req,res)=>{res.render("../views/register.ejs")});
 router.get('/login', (req,res)=>{res.render("../views/login.ejs")});
 router.get('/choice', (req,res)=>{res.render("../views/choice.ejs")});
+router.get('/hackit/:id', async (req, res) => {
 
+
+    try {
+      const question = await Question.findById(req.params.id);
+      res.render('../views/mcq.ejs', {
+         question,
+         success : false,
+         fail : false });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
 
 router.get('/createhack', (req,res)=>{res.render("../views/createhack.ejs")});
 router.get('/hackit', async (req,res)=> {
@@ -27,17 +41,63 @@ router.get('/hackit', async (req,res)=> {
     }
     })
 
-router.get('/hackit/:id', async (req, res) => {
+      
+    // router.post('/check-answer', async (req, res) => {
+    //     console.log(req.body)
+    //     // const keys = Object.keys(req.body)
+    //     // try {
+    //     //   const question = await Question.findById(keys[0]);
+    //     //   const answer = question.data.correct;
+      
+    //     //   // check if answer is correct
+    //     //   if (req.body.selectedOption === answer) {
+    //     //     res.send('Correct answer!');
+    //     //   } else {
+    //     //     res.send('Incorrect answer!');
+    //     //   }
+    //     // } catch (err) {
+    //     //   console.error(err);
+    //     //   res.status(500).send('Internal Server Error');
+    //     // }
+    //   });
+
+
+
+router.post('/check-answer/:id', async (req, res) => {
+    console.log(req.body);
     try {
-        const post = await Post.findById(req.params.id);
-        res.render('post', { post });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    }
+        const questionId = req.params.id;
+        const userAnswer = req.body.answer;
+        
+        // find the question in the database
+        const question = await Question.findById(questionId);
+        console.log(userAnswer);
+        console.log(question);
+        
+        // check if the user's answer matches the correct answer
+        if (userAnswer === question.data[`${question.data.correct}`]) {
+            res.render('../views/mcq.ejs',{
+                question: question,
+                success : true,
+                fail : false
+            });
+            } else {
+            res.render('../views/mcq.ejs',{
+                question : question,
+                success : false,
+                fail : true
+            });
+            }
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        }
     });
       
 
+module.exports = router;
+
+      
 router.post('/createhack', (req, res) => {
     // Extract form data from the request
     const formData = req.body;
